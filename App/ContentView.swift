027037e72224bf9420157d50a422aa12f47f775a
ElMajadiOussama <ElMajadiOussama>
2023-04-02 01:23:47 +0200

    Ajout de la barre de navigation et implémentation de la liste horizontale d'auteurs sur la vue Home

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
         TabView{
             NavigationView{
                 Homeview ()
+                    .navigationBarTitle("My app", displayMode: .inline)
                     .toolbar {
                         ToolbarItemGroup(placement: .navigationBarLeading) {
                             Button{
@@ -29,7 +30,10 @@ struct ContentView: View {
                             }
                         }
                     }
-                    
+            }
+            .tabItem{
+            Image(systemName: "house.fill")
+                Text("Home")
             }
         }
     }
@@ -37,7 +41,28 @@ struct ContentView: View {
     
     struct Homeview: View {
         var body: some View {
-            Text("Home")
+            ScrollView { // Création d'une ScrollView
+                LazyVStack(alignment: .leading, spacing: 0) { //Organise une liste d'éléments verticalement et ne charge que les éléments qui sont visibles à l'écran pour une meilleure performance.
+                    ScrollView(.horizontal, showsIndicators: false) { //Crée une vue scrollable pour afficher des éléments au-delà de la taille de l'écran.
+                        LazyHStack(spacing: 20) { //Conteneur qui organise les vues horizontalement en chargeant uniquement celles qui sont nécessaires à l'affichage
+                            ForEach(Author.all) { author in // Itération sur tous les éléments d'un tableau Author.all
+                                VStack { // Création d'une VStack, organise ses sous-vues en une pile verticale.
+                                    Image(author.photoName)
+                                        .resizable() // Rendre l'image redimensionnable
+                                        .scaledToFit() // Redimensionner l'image pour tenir dans son conteneur
+                                        .clipShape(Circle()) // Clipper l'image en forme de cercle
+                                        .frame(width: 70) // Définir une largeur pour l'image
+                                        .overlay( // Ajout d'un calque sur l'image
+                                            Circle().stroke(Color.red, // Ajout d'un bord rouge en forme de cercle
+                                                            style: StrokeStyle(lineWidth: 2))
+                                        )
+                                    Text(author.name) //
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     struct ContentView_Previews: PreviewProvider {
