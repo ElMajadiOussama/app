7273c350c9d56cd035e5657d665ad0b9c21874bc
ElMajadiOussama <ElMajadiOussama>
2023-04-01 21:37:38 +0200

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Oussama Elmajadi on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
